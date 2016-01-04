96d65f63c8d1300b96280bbeb0abed187576e1b1
takumori <takumori@dcs.co.jp>
2016-01-04 13:53:21 +0900

    test

--- WEB-INF/HelloWorld.java
+++ WEB-INF/HelloWorld.java
@@ -4,18 +4,17 @@ import javax.servlet.http.*;
 
 public class HelloWorld extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException{
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Hello World!</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Hello World!</h1>");
-        out.println("</body>");
-        out.println("</html>");
-    }
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello World!</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hello World! Hello World!</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
 }
